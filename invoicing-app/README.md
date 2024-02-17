47891b05e031e4d0abaa4a8e5ce5b59add5da1aa
Jay Hayes <jayhayes@192.168.1.192>
2024-02-17 14:10:11 +0000

    Final time complete commit. Started implementing the createInvoice form but ran out of time before I could produce anything substancial. Adding README.

---
+++ invoicing-app/README.md
@@ -0,0 +1,8 @@
+## Thoughts and Explanation of My Final Product
+
+I'm adding a short README before I push my final result of this tech test up.
+
+Honestly, I'm a little disappointed with how much of it I've managed to complete in the time given.
+I'd like to have had a bit more time to plot it out in the first instance before the timer started as I feel like I ended up using too much time working out what I could feasibly make in the time allowed.
+I knew I would have to be very scant on styling in order to get any functionality down, but by the time I'd set up the Expo app and wire-framed a rough idea of how I'd put it together and got all the dependencies sorted I was already low on time.
+I didn't get around to testing as there really wasn't enough functionality to properly test by the time I'd finished.
--- invoicing-app/src/components/CreateInvoiceForm.tsx
+++ invoicing-app/src/components/CreateInvoiceForm.tsx
@@ -1,18 +1,109 @@
-import { TextInput, View } from "react-native";
+import { TextInput, View, Button, Text } from "react-native";
+import { Controller, useForm } from "react-hook-form";
 
 export const CreateInvoiceForm = () => {
+  const addNewItem = () => {
+    // Add a new item to form.
+  };
+
+  const submitForm = () => {};
+
   return (
     <View>
-      <form>
-        <TextInput />
-        <TextInput />
-        <TextInput />
-        <TextInput />
-        <TextInput />
-        <TextInput />
-        <TextInput />
-        <TextInput />
-      </form>
+      <View>
+        <Text>New Invoice</Text>
+      </View>
+      <View>
+        <Text>Bill From</Text>
+        <TextInput
+          placeholder="Street Address"
+          /*  onChangeText={}
+        value={} */
+        />
+        <TextInput
+          placeholder="City"
+          /*  onChangeText={}
+        value={} */
+        />
+        <TextInput
+          placeholder="Post Code"
+          /* onChangeText={}
+                value={} */
+        />
+        <TextInput
+          placeholder="Country"
+          /*  onChangeText={}
+        value={} */
+        />
+        <Text>Bill To</Text>
+        <TextInput
+          placeholder="Client's Name"
+          /*  onChangeText={}
+        value={} */
+        />
+        <TextInput
+          placeholder="Client's Email"
+          /*  onChangeText={}
+        value={} */
+        />
+        <TextInput
+          placeholder="Street Address"
+          /*  onChangeText={}
+        value={} */
+        />
+        <TextInput
+          placeholder="City"
+          /*  onChangeText={}
+        value={} */
+        />
+        <TextInput
+          placeholder="Post Code"
+          /*  onChangeText={}
+        value={} */
+        />
+        <TextInput
+          placeholder="Country"
+          /*  onChangeText={}
+        value={} */
+        />
+        <TextInput
+          placeholder="Invoice Date"
+          /*  onChangeText={}
+        value={} */
+        />
+        <TextInput
+          placeholder="Payment Terms"
+          /*  onChangeText={}
+        value={} */
+        />
+        <TextInput
+          placeholder="Project Description"
+          /*  onChangeText={}
+        value={} */
+        />
+        <Text>Item List</Text>
+        <TextInput
+          placeholder="Item Name"
+          /*  onChangeText={}
+        value={} */
+        />
+        <TextInput
+          placeholder="Item Name"
+          /*  onChangeText={}
+        value={} */
+        />
+        <TextInput
+          placeholder="Qty."
+          /*  onChangeText={}
+        value={} */
+        />
+        <TextInput
+          placeholder="Price"
+          /*  onChangeText={}
+        value={} */
+        />
+        <Button title="+ Add New Item" onPress={addNewItem} />
+      </View>
     </View>
   );
 };
